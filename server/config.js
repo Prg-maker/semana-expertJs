77e5b4c6d8393e715f9ba2cfbac5887ad9ffa97b
Prg-maker <df3296709@gmail.com>
2022-03-14 16:04:05 -0300

    init project

---
+++ server/config.js
@@ -0,0 +1,9 @@
+import {join , dirname}from 'path'
+import {fileURLToPath} from 'url'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
+
+
+const root = join(currentDir, '../')
+const audio = join(currentDir, '../')
